1d5fb238c66a90439b7358728a9230e2e1882c5c
Mohan <mohankarki86555@gmail.com>
2025-03-12 12:08:45 +0545

    2nd day

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -6,10 +6,10 @@ export const generateToken =(userId, res) => {
     })
 
     res.cookie("jwt", token, {
-        maxAge: 7 * 24 * 60 * 100,
+        maxAge: 7 * 24 * 60 * 60 * 1000,
         httpOnly: true,
-        sameSite: "strict",
-        secure: process.env.NODE_ENV !== "developmenr"
+        sameSite: "Lax",
+        secure: process.env.NODE_ENV !== "production"
     })
     return token;
 }
