61f094ca00159f15ecd1ab04b22a1de3990596d7
Mohan <mohankarki86555@gmail.com>
2025-03-13 09:50:14 +0545

    final

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,32 +1,50 @@
-import express from "express"
-import dotenv from "dotenv"
-import cookieParser from "cookie-parser"
-import cors from "cors"
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+import path from "path";
+
+import { connectDB } from "./lib/db.js";
+
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+import { app, server } from "./lib/socket.js";
+
+import dotenv from 'dotenv';
+dotenv.config();
+console.log("PORT:", process.env.PORT);
+console.log("Cloudinary Cloud Name:", process.env.CLOUDINARY_CLOUD_NAME);
+console.log("Cloudinary API Key:", process.env.CLOUDINARY_API_KEY);
+console.log("Cloudinary API Secret:", process.env.CLOUDINARY_API_SECRET);
 
 
-import {connectDB}  from "./lib/db.js"
+const PORT = process.env.PORT;
+const __dirname = path.resolve();
 
-import authRoutes from "./routes/auth.route.js"
-import messageRoutes from "./routes/message.route.js"
+app.use(express.json({ limit: "50mb" }));  
+app.use(express.urlencoded({ limit: "50mb", extended: true }));  
 
-
-dotenv.config()
-const app = express()
-
-const PORT = process.env.PORT
-
-app.use(express.json())
-app.use(cookieParser())
-app.use(cors({
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+  cors({
     origin: "http://localhost:5173",
-    credentials: true
-}))
+    credentials: true,
+  })
+);
 
-app.use("/api/auth", authRoutes)
-app.use("/api/message", messageRoutes)
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-app.listen(PORT, () => {
-    console.log("Server is starting on PORT:" + PORT )
-    connectDB()
-})
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
+
+server.listen(PORT, () => {
+  console.log("server is running on PORT:" + PORT);
+  connectDB();
+});
--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { useState } from "react";
 import { useAuthStore } from "../store/useAuthStore";
 import { Eye, EyeOff, Loader2, Lock, Mail, MessageSquare, User } from "lucide-react";
@@ -37,10 +36,10 @@ const SignUpPage = () => {
 
     return (
         <div className="min-h-screen grid lg:grid-cols-2">
-
+            {/* left side */}
             <div className="flex flex-col justify-center items-center p-6 sm:p-12">
                 <div className="w-full max-w-md space-y-8">
-
+                    {/* LOGO */}
                     <div className="text-center mb-8">
                         <div className="flex flex-col items-center gap-2 group">
                             <div
